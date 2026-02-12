f19560218bb87cec06311d8c43b538917c883f87
Kalana Ratnayake <kalanaratnayake95@gmail.com>
2026-02-12 16:18:11 +0000

    fixed most compile errors, removed tinyxml2, event_logger from runner dependencies and updated the api to match new event subsystem

--- include/fabric_capabilities/completion_runner.hpp
+++ include/fabric_capabilities/completion_runner.hpp
@@ -1,6 +1,5 @@
 #pragma once
 #include <string>
-#include <tinyxml2.h>
 #include <pluginlib/class_list_macros.hpp>
 #include <capabilities2_runner/service_runner.hpp>
 #include <fabric_msgs/srv/complete_fabric.hpp>
@@ -11,7 +10,7 @@ namespace capabilities2_runner
  * @brief completion runner
  *
  * This class is a wrapper around the capabilities2 service runner and is used to
- * call on the /fabric/set_completion service, providing it as a 
+ * call on the /fabric/set_completion service, providing it as a
  * capability that notifys the completion of the fabric
  */
 class FabricCompletionRunner : public ServiceRunner<fabric_msgs::srv::CompleteFabric>
@@ -27,22 +26,23 @@ public:
    * @param node shared pointer to the capabilities node. Allows to use ros node related functionalities
    * @param run_config runner configuration loaded from the yaml file
    */
-  virtual void start(rclcpp::Node::SharedPtr node, const runner_opts& run_config) override
+  virtual void start(rclcpp::Node::SharedPtr node, const runner_opts& run_config, const std::string& bond_id) override
   {
     init_service(node, run_config, "/fabric/set_completion");
+
+    // emit start event
+    emit_started(bond_id, param_on_started());
   }
 
 protected:
   /**
-   * @brief Generate a empty request.
-   *
-   * This function is used in conjunction with the trigger function to inject type erased parameters
-   * into the typed action
-   *
-   * @param parameters
-   * @return prompt_msgs::srv::Prompt::Request the generated request
+   * @brief generate a service request from the event parameters provided in the trigger function
+   * 
+   * @param parameters EventParameters containing parameters for the trigger event
+   * @return CompleteFabric::Request the service request to be sent to the service server
    */
-  virtual typename fabric_msgs::srv::CompleteFabric::Request generate_request(tinyxml2::XMLElement* parameters, int id) override
+  virtual typename fabric_msgs::srv::CompleteFabric::Request
+  generate_request(capabilities2_events::EventParameters& parameters) override
   {
     fabric_msgs::srv::CompleteFabric::Request request;
     return request;
--- include/fabric_capabilities/set_plan_runner.hpp
+++ include/fabric_capabilities/set_plan_runner.hpp
@@ -2,7 +2,6 @@
 
 #include <thread>
 
-#include <tinyxml2.h>
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp_action/rclcpp_action.hpp>
 
@@ -30,29 +29,36 @@ public:
    *
    * @param node shared pointer to the capabilities node. Allows to use ros node related functionalities
    * @param run_config runner configuration loaded from the yaml file
+   * @param bond_id bond id for the runner instance, used for event emission and tracking
    */
-  virtual void start(rclcpp::Node::SharedPtr node, const runner_opts& run_config) override
+  virtual void start(rclcpp::Node::SharedPtr node, const runner_opts& run_config, const std::string& bond_id) override
   {
     init_service(node, run_config, "/fabric/set_plan");
+
+    // emit start event
+    emit_started(bond_id, param_on_started());
   }
 
 protected:
   /**
-   * @brief This generate goal function overrides the generate_goal() function from ActionRunner()
-   * @param parameters XMLElement that contains parameters in the format
-   * @return ActionT::Goal the generated goal
+   * @brief Generate service request from event parameters
+   *
+   * This function is used to create a service request based on the parameters received from the trigger event. It
+   * extracts necessary information from the parameters and populates the service request accordingly.
+   * 
+   * @param parameters EventParameters containing parameters from the trigger event
+   * @return fabric_msgs::srv::SetFabricPlan::Request populated service request to be sent to the service server
    */
-  virtual fabric_msgs::srv::SetFabricPlan::Request generate_request(tinyxml2::XMLElement* parameters, int id) override
+  virtual fabric_msgs::srv::SetFabricPlan::Request
+  generate_request(capabilities2_events::EventParameters& parameters) override
   {
-    tinyxml2::XMLElement* planElement = parameters->FirstChildElement("ReceievdPlan");
-
     fabric_msgs::srv::SetFabricPlan::Request request;
+    request.plan = "";
 
-    // Check if the element was found and has text content
-    if (planElement && planElement->GetText())
-    {
-      request.plan = std::string(planElement->GetText());
-    }
+    if (parameters.has_value("ReceivedPlan"))
+      request.plan = std::any_cast<std::string>(parameters.get_value("ReceivedPlan"));
+    else
+      RCLCPP_WARN(node_->get_logger(), "No 'ReceivedPlan' parameter found in event parameters. Sending empty plan.");
 
     return request;
   }
